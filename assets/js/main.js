4cf64934a66fa0eddaead97df0eeee336bc102f1
Mel-SON <omelyanchenko.semen@yandex.ru>
2022-06-12 15:36:16 +0300

    Добавляет очистку полей после успешного создания продукта

--- assets/js/main.js
+++ assets/js/main.js
@@ -118,6 +118,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
                             if (resp.link != true) {
                                 document.querySelector('#result').innerHTML = '<a href="' + resp.link + '">Смотреть ' + document.querySelector('#product_name').value + '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="40" height="20" fill="#0027ff"><path d="M502.6 278.6l-128 128c-12.51 12.51-32.76 12.49-45.25 0c-12.5-12.5-12.5-32.75 0-45.25L402.8 288H32C14.31 288 0 273.7 0 255.1S14.31 224 32 224h370.8l-73.38-73.38c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l128 128C515.1 245.9 515.1 266.1 502.6 278.6z"/></svg></a>';
+                                product_name.value = '';
+                                price.value = '';
+                                date_create.value = '';
+                                newimg.value = '';
+                                type_of_product.value = '';
                             } else {
                                 document.querySelector('#result').innerHTML = 'Что-то пошло не так...попробуйте снова';
                             }
